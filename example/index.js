e5953cfae424304511c1ed680016b7e9fab57aae
Crux <crux153@users.noreply.github.com>
2017-09-20 16:01:23 +0900

    Change code style of example

--- example/index.js
+++ example/index.js
@@ -1,6 +1,8 @@
 const coap = require("coap");
 const app = require("./app");
+
 const server = coap.createServer(app);
+
 server.listen(() => {
-    console.log("The CoAP server is now running.\n" + app.help);
+	console.log("The CoAP server is now running.\n" + app.help);
 });
--- example/routes/thermometer.js
+++ example/routes/thermometer.js
@@ -4,55 +4,50 @@ const router = Router();
 const thermometer = require("../sensors/thermometer");
 
 router.get("/", (req, res) => {
-    writeJSON(res, {
-        temperature: thermometer.temperature,
-        humidity: thermometer.humidity,
-        timestamp: new Date().getTime()
-    });
-    res.end();
+	writeJSON(res, {
+		temperature: thermometer.temperature,
+		humidity: thermometer.humidity,
+		timestamp: new Date().getTime()
+	});
+	res.end();
 });
 
 router.observe("/", (req, res) => {
-    function _onupdate()
-    {
-        writeJSON(res, {
-            temperature: thermometer.temperature,
-            humidity: thermometer.humidity,
-            timestamp: new Date().getTime()
-        });
-    }
+	function _onupdate() {
+		writeJSON(res, {
+			temperature: thermometer.temperature,
+			humidity: thermometer.humidity,
+			timestamp: new Date().getTime()
+		});
+	}
 
-    console.log("Start observing...");
-    thermometer.on("update", _onupdate);
-    res.on("finish", err => {
-        thermometer.removeListener("update", _onupdate);
-        console.log("End observing.");
-    });
+	console.log("Start observing...");
+	thermometer.on("update", _onupdate);
+	res.on("finish", () => {
+		thermometer.removeListener("update", _onupdate);
+		console.log("End observing.");
+	});
 });
 
 router.get("/temperature", (req, res) => {
-    writeJSON(res, {
-        temperature: thermometer.temperature,
-        timestamp: new Date().getTime()
-    });
-    res.end();
+	writeJSON(res, {
+		temperature: thermometer.temperature,
+		timestamp: new Date().getTime()
+	});
+	res.end();
 });
 
 router.get("/humidity", (req, res) => {
-    writeJSON(res, {
-        humidity: thermometer.humidity,
-        timestamp: new Date().getTime()
-    });
-    res.end();
+	writeJSON(res, {
+		humidity: thermometer.humidity,
+		timestamp: new Date().getTime()
+	});
+	res.end();
 });
 
-
-
-
-function writeJSON(res, json)
-{
-    res.setOption("Content-Format", "application/json");
-    res.write(JSON.stringify(json));
+function writeJSON(res, json) {
+	res.setOption("Content-Format", "application/json");
+	res.write(JSON.stringify(json));
 }
 
 module.exports = router;
